7cf1b8e8d1dd4689b89ee67a2fa464f6d5438e04
Dikshit Mahanot <168990083+dikjain@users.noreply.github.com>
2025-10-05 16:32:25 +0530

    Update RewardDisplay calculation in dashboard and add ESLint directive in Masonry component.

--- components/Masonry.tsx
+++ components/Masonry.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 import React, { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 
