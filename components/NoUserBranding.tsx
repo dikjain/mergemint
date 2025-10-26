28522e422cc46be46f128c3741547c5285f966d1
Dikshit Mahanot <168990083+dikjain@users.noreply.github.com>
2025-10-27 01:47:44 +0530

    Update NoUserBranding component to change text from "EARN SOLANA" to "EARN SOLs"

--- components/NoUserBranding.tsx
+++ components/NoUserBranding.tsx
@@ -26,7 +26,7 @@ export default function noUserBranding() {
             className="text-6xl font-bold text-neutral-700 font-nunito"
           >
             MAKE PR,
-            <br /> EARN SOLANA
+            <br /> EARN SOL
           </h1>
         </div>
 
